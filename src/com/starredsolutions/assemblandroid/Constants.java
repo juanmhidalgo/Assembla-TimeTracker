44a387f0d0fee07a045e3474673411e97eab621e
Juan M. Hidalgo <juanmhidalgo@gmail.com>
2012-01-14 12:20:42 -0300

    Added Constants for future use

--- src/com/starredsolutions/assemblandroid/Constants.java
+++ src/com/starredsolutions/assemblandroid/Constants.java
@@ -8,6 +8,8 @@ package com.starredsolutions.assemblandroid;
  *
  */
 public class Constants {
+	public static boolean DEVELOPER_MODE = true;
+	
 	/**
      * Account type string.
      */
@@ -17,4 +19,16 @@ public class Constants {
      * Authtoken type string.
      */
     public static final String AUTHTOKEN_TYPE ="com.starredsolutions.assembla";
+    
+    public static final String USERID_KEY ="USER_ID";
+    public static final String LAST_SYNC_KEY ="LAST_SYNC";
+    
+    public static final long MIN_TIME_BW_SYNC = 15 * 60 *1000; //15 min
+    
+    /*
+    public static boolean SUPPORTS_GINGERBREAD = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD;
+    public static boolean SUPPORTS_HONEYCOMB = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB;
+    public static boolean SUPPORTS_FROYO = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.FROYO;*/
+    public static boolean SUPPORTS_ECLAIR = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR;
+    
 }
