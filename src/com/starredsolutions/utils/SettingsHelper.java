fabff36de1974e727344156cc7d8c55703bc7714
Juan M. Hidalgo <juanmhidalgo@gmail.com>
2012-01-14 12:18:36 -0300

    Added useful methods

--- src/com/starredsolutions/utils/SettingsHelper.java
+++ src/com/starredsolutions/utils/SettingsHelper.java
@@ -6,6 +6,7 @@ package com.starredsolutions.utils;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 /**
  * @author Juan M. Hidalgo <juan@starredsolutions.com.ar>
@@ -13,6 +14,7 @@ import android.preference.PreferenceManager;
  */
 public class SettingsHelper {
 	private static String TAG = "SettingsHelper";
+	private static final boolean LOGV = Log.isLoggable(TAG, Log.VERBOSE);
 	private static SettingsHelper instance = null;
 	private SharedPreferences sp;
 	private Context context;
@@ -27,6 +29,10 @@ public class SettingsHelper {
 			instance = new SettingsHelper(ctx);
 		}
 		return instance;
+	}
+
+	public boolean containsKey(String key){
+		return sp.contains(key);
 	}
 	
 	public String getString(String key,String defaultValue){
@@ -47,5 +53,51 @@ public class SettingsHelper {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param key
+	 * @param defaultValue
+	 * @return
+	 */
+	public long  getLong(String key,long defaultValue){
+		try{
+			return sp.getLong(key, defaultValue);
+		}catch(ClassCastException e){
+			String val;
+			val = sp.getString(key, null);
+			if(val != null){
+				return Long.parseLong(val);
+			}else{
+				return defaultValue;
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void putString(final String key,final String value){
+		if(LOGV) Log.v(TAG,"setString(key=" + key + ",value=" + value + ")");
+		(new Thread(new Runnable() {
+			public void run() {
+				sp.edit().putString(key, value).commit();
+			}
+		})).start();
+	}
 	
+	/**
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	public void putLong(final String key,final Long value){
+		if(LOGV) Log.v(TAG,"setString(key=" + key + ",value=" + value + ")");
+		(new Thread(new Runnable() {
+			public void run() {
+				sp.edit().putLong(key, value).commit();
+			}
+		})).start();
+	}
 }
