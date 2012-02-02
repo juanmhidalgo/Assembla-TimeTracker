d4c97a38774397bb92e430f546181c4bde79cabd
Juan M. Hidalgo <juanmhidalgo@gmail.com>
2012-02-02 02:56:42 -0300

    Added Current Task To Dashboard

--- src/com/starredsolutions/assemblandroid/ui/fragments/DashboardFragment.java
+++ src/com/starredsolutions/assemblandroid/ui/fragments/DashboardFragment.java
@@ -10,26 +10,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.starredsolutions.assemblandroid.R;
+import com.starredsolutions.assemblandroid.TimeTrackerApplication;
+import com.starredsolutions.assemblandroid.models.Task;
 import com.starredsolutions.assemblandroid.provider.AssemblaContract.Spaces;
+import com.starredsolutions.assemblandroid.provider.AssemblaContract.Tickets;
 
 /**
  * @author Juan M. Hidalgo <juan@starredsolutions.com.ar>
  *
  */
 public class DashboardFragment extends Fragment {
-
+	Task mCurrentTask;
+	Button btnTask;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		
+		
 		View root = inflater.inflate(R.layout.fragment_dashboard, container);
 		root.findViewById(R.id.btnSpaceList).setOnClickListener(new OnClickListener() {
-			
 			public void onClick(View v) {
 				startActivity(new Intent(Intent.ACTION_VIEW, Spaces.CONTENT_URI));
 			}
 		});
 		
+		btnTask = (Button) root.findViewById(R.id.btnCurrentTask); 
+		btnTask.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				if(mCurrentTask != null){
+					startActivity(new Intent(Intent.ACTION_VIEW,Tickets.buildTicketUri(Long.toString(mCurrentTask.get_id()))));
+				}
+			}
+		});
+
+		
 		return root;
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		mCurrentTask = ((TimeTrackerApplication) getActivity().getApplicationContext()).getCurrentTask();
+		if(mCurrentTask != null){
+			btnTask.setVisibility(View.VISIBLE);
+		}else{
+			btnTask.setVisibility(View.GONE);
+		}
 	}
 }
